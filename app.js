6c5bc7e2dfb67a29f791e325bc54c32eaaf80af8
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-10-25 14:55:35 -0400

    Created website base

---
+++ app.js
@@ -0,0 +1,10 @@
+gsap.from(".img", {
+	opacity: 0, delay: 2, duration: 3, x: 50, stagger:0.9
+} )
+gsap.from(".img-second", {
+  opacity: 0,
+  delay: 4,
+  duration: 3,
+  x: 50,
+  stagger: 0.9,
+});
